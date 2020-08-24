c1f5afe4a2e35a7e48eaa26faa47b632d96fe3b4
ivanburda1986 <burda.ivan@gmail.com>
2020-08-24 23:40:35 +0200

    added https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Get info about a place
 async function getPlaceData() {
-  const response = await fetch(`http://www.wienerlinien.at/ogd_realtime/monitor?stopId=147&activateTrafficInfo=stoerungkurz&activateTrafficInfo=stoerunglang&activateTrafficInfo=aufzugsinfo`, {
+  const response = await fetch(`https://www.wienerlinien.at/ogd_realtime/monitor?stopId=147&activateTrafficInfo=stoerungkurz&activateTrafficInfo=stoerunglang&activateTrafficInfo=aufzugsinfo`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
