e34a6ccc8ce928b26af6ce485c0af4437e49390c
ivanburda1986 <burda.ivan@gmail.com>
2020-08-24 23:39:31 +0200

    created the function to fetch from wiener linien

---
+++ script.js
@@ -0,0 +1,12 @@
+//Get info about a place
+async function getPlaceData() {
+  const response = await fetch(`http://www.wienerlinien.at/ogd_realtime/monitor?stopId=147&activateTrafficInfo=stoerungkurz&activateTrafficInfo=stoerunglang&activateTrafficInfo=aufzugsinfo`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'no-cors'
+    }
+  });
+  console.log(response.json());
+}
