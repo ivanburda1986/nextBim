0bfb013c626f22ec8fbc6d7e345923841f7b52f4
ivanburda1986 <burda.ivan@gmail.com>
2020-08-25 20:48:55 +0200

    http to https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Get info about a place
 async function getPlaceData() {
-  const response = await fetch(`http://www.wienerlinien.at/ogd_realtime/monitor?stopId=143`, {
+  const response = await fetch(`https://www.wienerlinien.at/ogd_realtime/monitor?stopId=143`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
