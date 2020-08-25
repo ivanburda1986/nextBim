1de126406f31a4ff9cb0259a4f45489c7bc2eb1f
ivanburda1986 <burda.ivan@gmail.com>
2020-08-26 00:44:56 +0200

    found a working proxy for solving cors

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 //Get info about a place
 async function getPlaceData() {
-  const response = await fetch(`https://www.wienerlinien.at/ogd_realtime/monitor?stopId=143`, {
+  const response = await fetch(`https://api.allorigins.win/get?url=http://www.wienerlinien.at/ogd_realtime/monitor?stopId=143`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
     }
   });
-  const data = await response.json();
+  const data = await response;
   console.log(data);
 }
