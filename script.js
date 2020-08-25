267a6b3fde452d97837ac8b630726102961a2a07
ivanburda1986 <burda.ivan@gmail.com>
2020-08-25 20:45:55 +0200

    testing the api

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //Get info about a place
 async function getPlaceData() {
-  const response = await fetch(`https://www.wienerlinien.at/ogd_realtime/monitor?stopId=147&activateTrafficInfo=stoerungkurz&activateTrafficInfo=stoerunglang&activateTrafficInfo=aufzugsinfo`, {
+  const response = await fetch(`http://www.wienerlinien.at/ogd_realtime/monitor?stopId=143`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
-      'Access-Control-Allow-Origin': 'https://ivanburda1986.github.io/nextBim/',
       'Content-Type': 'application/json'
     }
   });
-  console.log(response.json());
+  const data = await response.json();
+  console.log(data);
 }
